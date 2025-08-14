27153cccf50da309f5c0bb1565c72b2469813bfc
supercoder382001 <63428439+supercoder382001@users.noreply.github.com>
2025-08-15 04:37:09 +0530

    Update flight_types.ads

--- src/flight_types.ads
+++ src/flight_types.ads
@@ -4,32 +4,30 @@ with Ada.Containers.Vectors;
 
 package Flight_Types is
 
-   -- An Ada representation of an Airport record.
    type Airport_Record is record
       Name         : Ada.Strings.Unbounded.Unbounded_String;
       Location     : Ada.Strings.Unbounded.Unbounded_String;
       Max_Capacity : Positive;
    end record;
-   -- Overloads the standard Image attribute for convenient printing.
-   function Image(Item : Airport_Record) return String;
 
-   -- An Ada representation of a Controller record.
+   function Image (Item : Airport_Record) return String;
+
    type Controller_Record is record
       License_Number   : Ada.Strings.Unbounded.Unbounded_String;
       Name             : Ada.Strings.Unbounded.Unbounded_String;
       Experience_Years : Natural;
    end record;
-   function Image(Item : Controller_Record) return String;
 
-   -- An Ada representation of a Flight record.
+   function Image (Item : Controller_Record) return String;
+
    type Flight_Record is record
-      Identifier        : Ada.Strings.Unbounded.Unbounded_String;
-      Origin_Name       : Ada.Strings.Unbounded.Unbounded_String;
-      Destination_Name  : Ada.Strings.Unbounded.Unbounded_String;
+      Identifier       : Ada.Strings.Unbounded.Unbounded_String;
+      Origin_Name      : Ada.Strings.Unbounded.Unbounded_String;
+      Destination_Name : Ada.Strings.Unbounded.Unbounded_String;
    end record;
-   function Image(Item : Flight_Record) return String;
 
-   -- Generic vector packages to hold lists of our records.
+   function Image (Item : Flight_Record) return String;
+
    package Airport_Vectors is new Ada.Containers.Vectors
      (Index_Type => Positive, Element_Type => Airport_Record);
 
