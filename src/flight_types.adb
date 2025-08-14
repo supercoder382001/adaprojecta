1e4580735803b322479af6710fd9995ad78573ff
supercoder382001 <63428439+supercoder382001@users.noreply.github.com>
2025-08-15 04:47:16 +0530

    Update flight_types.adb

--- src/flight_types.adb
+++ src/flight_types.adb
@@ -12,7 +12,8 @@ package body Flight_Types is
    function Image (Item : Controller_Record) return String is
    begin
       return "- " & Ada.Strings.Unbounded.To_String (Item.Name) &
-             " (Lic: " & Ada.Strings.Unbounded.To_String (Item.License_Number) &
+             " (Lic: " & 
+             Ada.Strings.Unbounded.To_String (Item.License_Number) &
              ", Exp: " & Natural'Image (Item.Experience_Years) & " yrs)";
    end Image;
 
