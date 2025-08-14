8d9a0e7404012e535a726c0958ebd4fd6fb856de
supercoder382001 <63428439+supercoder382001@users.noreply.github.com>
2025-08-15 04:05:10 +0530

    Update database_operations.ads

--- src/database_operations.ads
+++ src/database_operations.ads
@@ -3,7 +3,7 @@ with ADO.Sessions;
 with Flight_Types;
 
 package Database_Operations is
-   Database_Error, Record_Not_Found, Duplicate_Record, Invalid_Input : 
+   Database_Error, Record_Not_Found, Duplicate_Record, Invalid_Input :
      exception;
 
    procedure Initialize_Database_Connection;
@@ -12,23 +12,23 @@ package Database_Operations is
 
    procedure Clear_All_Data;
 
-   procedure Add_Airport (Name : String; Location : String; 
+   procedure Add_Airport (Name : String; Location : String;
                          Max_Capacity : Positive);
    function List_Airports return Flight_Types.Airport_Vectors.Vector;
-   procedure Update_Airport (Old_Name, New_Name, New_Location : String; 
+   procedure Update_Airport (Old_Name, New_Name, New_Location : String;
                             New_Capacity : Positive);
    procedure Delete_Airport (By_Name : String);
 
    procedure Add_Controller (License, Name : String; Experience : Natural);
    function List_Controllers return Flight_Types.Controller_Vectors.Vector;
-   procedure Update_Controller (Old_License, New_Name : String; 
+   procedure Update_Controller (Old_License, New_Name : String;
                                New_Experience : Natural);
    procedure Delete_Controller (By_License : String);
 
-   procedure Add_Flight (Identifier, Origin_Airport_Name, 
+   procedure Add_Flight (Identifier, Origin_Airport_Name,
                         Dest_Airport_Name : String);
    function List_Flights return Flight_Types.Flight_Vectors.Vector;
-   procedure Update_Flight (Old_Identifier, New_Origin_Name, 
+   procedure Update_Flight (Old_Identifier, New_Origin_Name,
                            New_Dest_Name : String);
    procedure Delete_Flight (By_Identifier : String);
 
